662378511f48e8dbd8314f809b61bf7163be7c28
Uros Karic <dot.kadza@gmail.com>
2019-10-09 12:21:04 +0200

    Added intructions

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>This should be a exercisse in creating and managing Git Repos</h1>
+      <p>To enter add your git handle !</p>
+      <ul>
+        <li>@ukaric</li>
+      </ul>
     </div>
   );
 }
