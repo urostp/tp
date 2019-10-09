e9a246a90a9490719cc28f5dce6b07f318a77efd
Uros Karic <dot.kadza@gmail.com>
2019-10-09 12:34:16 +0200

    Added react router

--- src/App.js
+++ src/App.js
@@ -1,16 +1,30 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
-
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 function App() {
   return (
-    <div className="App">
-      <h1>This should be a exercisse in creating and managing Git Repos</h1>
-      <p>To enter add your git handle !</p>
-      <ul>
-        <li>@ukaric</li>
-      </ul>
-    </div>
+    <Router>
+      <div className="App">
+        <h1>This should be a exercisse in creating and managing Git Repos</h1>
+        <p>To enter add your git handle !</p>
+        <nav>
+          <ul>
+            <li>
+              <link to="/about">About</link>
+            </li>
+          </ul>
+        </nav>
+
+        <Switch>
+          <Route path="/about">
+            <div>About</div>
+          </Route>
+        </Switch>
+        <ul>
+          <li>@ukaric</li>
+        </ul>
+      </div>
+    </Router>
   );
 }
 
